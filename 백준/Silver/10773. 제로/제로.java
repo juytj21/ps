c1ba5d87603c297ecd9e5d509bc5f22bc6256ba9
juytj21 <juytj21@gmail.com>
2025-01-15 23:39:08 +0900

    [Silver IV] Title: 제로, Time: 692 ms, Memory: 113360 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int k = s.nextInt();
+
+        int [] num = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            num[i] = s.nextInt();
+        }
+
+        int sum = 0;
+        int [] list = new int[k];
+        int c = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (num[i] != 0) {
+                sum += num[i];
+                list[c] = num[i];
+                c++;
+            } else if (num[i] == 0) {
+                c--;
+                sum -= list[c];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
