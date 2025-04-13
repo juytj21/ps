46a15e1b2980bcea47c5756454f2f69fcefdd9fa
juytj21 <juytj21@gmail.com>
2025-04-13 22:47:42 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -6,14 +6,13 @@ class Solution {
         int[] answer = new int [len];
         
         for (int a = 0; a < len; a++) {
-            int [] p = array.clone();
             int i = commands[a][0];
             int j = commands[a][1];
             int k = commands[a][2];
             
             int [] arr = new int [j-i+1];
             for (int b = 0; b < j-i+1; b++) {
-                arr[b] = p[b+i-1];
+                arr[b] = array[b+i-1];
             }
             Arrays.sort(arr);
             
