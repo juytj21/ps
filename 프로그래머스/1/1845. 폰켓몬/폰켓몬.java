73ff23ddc392f7250a5897fc94e90db43cc442e0
juytj21 <juytj21@gmail.com>
2025-05-22 14:44:52 +0900

    [level 1] Title: 폰켓몬, Time: 3.13 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,9 +5,7 @@ class Solution {
         int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i])+1);
-            } else {
+            if (!map.containsKey(nums[i])) {
                 map.put(nums[i], 1);
             }
         }
